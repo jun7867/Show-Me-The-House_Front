d63f6e99a94c368d4a06baf502eb686d0d984711
박호현 <hohy@baghohyeon-ui-Macmini.local>
2021-05-26 14:39:04 +0900

    Add favorite sql file

--- db/favorite.sql
+++ db/favorite.sql
@@ -1,5 +1,3 @@
-SELECT * FROM happyhouse.housedeal;
-
 create table favorite(
 	fav_no int not null auto_increment,
     user_no int not null,
@@ -8,9 +6,3 @@ create table favorite(
     foreign key(user_no) references member(user_no) on update cascade on delete cascade,
     foreign key(housedeal_no) references housedeal(no) on update cascade on delete cascade
 );
-
-select h.no, h.dong, h.AptName, h.dealAmount, h.buildYear, h.dealYear, h.dealMonth, h.dealDay, h.floor
-from favorite f, housedeal h
-where h.no in (select housedeal_no from favorite where user_no = 1);
-
-insert into favorite(user_no, housedeal_no) value(1, 2);
